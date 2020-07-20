175df08e4c118ed520fc5d59ded64aa9bc4ca39a
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-07-20 16:14:30 -0400

    packaging

---
+++ astropy_sustainability/ads_helpers.py
@@ -0,0 +1,58 @@
+# Standard library
+import datetime
+
+# Third-party
+import ads
+import astropy.table as at
+import numpy as np
+
+
+def get_papers(query, rows_per_page=100, max_pages=100):
+    """
+    Parameters
+    ----------
+    query : str
+    rows_per_page : int (optional)
+    max_pages : int (optional)
+
+    Returns
+    -------
+    search_query : `ads.SearchQuery`
+    papers_df : `pandas.DataFrame`
+    """
+    q = ads.SearchQuery(q=query,
+                        sort="date",
+                        max_pages=max_pages,
+                        rows=rows_per_page,
+                        fl=["identifier", "title", "author", "year",
+                            "pubdate", "pub", "citation_count"])
+
+    all_dicts = []
+    for paper in q:
+        # Get arxiv ID
+        aid = [":".join(t.split(":")[1:]) for t in paper.identifier
+               if t.startswith("arXiv:")]
+
+        all_dicts.append(dict(
+            authors='; '.join(paper.author) if paper.author else '',
+            year=int(paper.year) if paper.year is not None else -1,
+            pubdate=paper.pubdate if paper.pubdate is not None else '',
+            title=paper.title[0],
+            pub=paper.pub,
+            arxiv=aid[0] if len(aid) else '',
+            citations=(paper.citation_count
+                       if paper.citation_count is not None else 0)
+        ))
+
+    papers = at.Table(sorted(all_dicts, key=lambda x: x['pubdate'],
+                             reverse=True))
+
+    # Convert list of papers to pandas DataFrame
+    df = papers.to_pandas()
+    df['pubdate'] = np.array([datetime.datetime(int(x.split('-')[0]),
+                                                max(1, int(x.split('-')[1])), 1)
+                              for x in df['pubdate']])
+    df.index = df['pubdate']
+    df = df.sort_index()
+
+    return q, df
---
+++ scripts/query_ads.py
@@ -0,0 +1,30 @@
+"""Note: need to store ADS_DEV_KEY on CI"""
+
+# Standard library
+import os
+
+# This project
+from astropy_sustainability import get_papers
+
+queries = {
+    'astropy-citations': (
+        'property:refereed citations(2018AJ....156..123A) '
+        'OR citations(2013A&A...558A..33A)'),
+    'python-fulltext': (
+        'full:"python" property:"refereed" collection:"astronomy"'),
+    'idl-fulltext': (
+        'full:"idl" property:"refereed" collection:"astronomy"'),
+    'matlab-fulltext': (
+        'full:"matlab" property:"refereed" collection:"astronomy"'),
+    'julia-fulltext': (
+        'full:"julia" property:"refereed" collection:"astronomy"')
+}
+
+# One up from the scripts directory:
+cache_path = os.path.join(os.path.split(os.path.abspath(__file__))[0],
+                          '../cache')
+os.makedirs(cache_path, exist_ok=True)
+
+for slug, query in queries.items():
+    q, papers_df = get_papers(query, rows_per_page=1000, max_pages=100)
+    papers_df.to_csv(os.path.join(cache_path, f'{slug}.csv'))
